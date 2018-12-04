711b8d2d1ef6eb6ee46b020a66096dc667a78c5a
ouailmj <ouailmjahedooo@gmail.com>
2018-12-04 17:00:09 +0000

    ajout close boutton

--- src/client/ChatMain.java
+++ src/client/ChatMain.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class ChatMain extends Application {
 
@@ -18,6 +19,7 @@ public class ChatMain extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Say Hi!");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         chatClient = new ChatClient();
